0edfb97a4882d14b798c4f269bb13f44f7d4c189
jihyun-N <gus2726@naver.com>
2023-12-28 15:25:49 +0900

    test

---
+++ test.js
@@ -0,0 +1,48 @@
+// switch
+
+// switch(평가) {           if(평가 == A) {
+//case A :               // A일때 코드
+// A일때 코드           } else if(평가 == B) {
+//case B :               // B일때 코드
+// B일때 코드           }
+//}
+
+// 사과 : 100원
+// 바나나 : 200원
+// 키위 : 300원
+// 멜론 : 500원
+// 수박 : 500원
+// 사고 싶은 과일을 물어보고 가격 알려주기
+
+// let fruit = prompt('무슨 과일을 사고 싶나요?');
+
+// switch (fruit) {
+//     case '사과':
+//         console.log('100원 입니다.');
+//         break;
+//     case '바나나':
+//         console.log('200원 입니다.');
+//         break;
+//     case '키위':
+//         console.log('300원 입니다.');
+//         break;
+//     case '멜론':
+//     case '수박':
+//         console.log('500원 입니다.');
+//         break;
+//         default :
+//         console.log('그런 과일은 없습니다.');
+
+// }
+
+
+// 함수 (function)
+
+//함수 작성
+// 펑션(함수), mule (함수명)( 여기안에는 변수 )
+
+// function mule(){
+//     alert('뮬 입니다.');
+//   }
+  
+//   mule();
